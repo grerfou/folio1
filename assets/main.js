7dda5701f62ac5914bf559a75b9bdbfea36fff50
gerfou <96663466+grerfou@users.noreply.github.com>
2023-12-01 16:55:14 +0100

    vite

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
